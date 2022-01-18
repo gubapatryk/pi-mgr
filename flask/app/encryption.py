e1569ba6d4b7ee358182d76ff6d363bc4557ab39
gubapatryk <gubapatryk@gmail.com>
2022-01-18 12:49:29 +0100

    Pepper

--- flask/app/encryption.py
+++ flask/app/encryption.py
@@ -1,13 +1,15 @@
 import bcrypt
 
+pepper = '2hf27hf283bf8'
+
 def encrypt(password):
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt + pepper)
     return hashed, salt
 
 def check_password(salt, password, hash):
-    return hash == encrypt_with_salt(password, salt)
+    return hash == encrypt_with_salt(password, salt + pepper)
 
 def encrypt_with_salt(password, salt):
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+    return bcrypt.hashpw(password.encode('utf-8'), salt + pepper)
 
